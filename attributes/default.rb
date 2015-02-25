98e93a3d58037e4ff49a05aa5205ca4287bc00ea
stephaneLII <stephane.lii@informatique.gov.pf>
2015-02-25 09:26:43 -1000

    Ajustement des attributs par défaut.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['postgresql']['database_root_password'] = 'postgres'
 default['postgresql']['config']['listen_addresses'] = '*'
-default['chef-postgresql']['databases'] = { 'db1' => { 'user' => 'user1', 'password' => 'pwd1' } , 'db2' => { 'user' => 'user2' , 'password' => 'pwd2' } }
+default['chef-postgresql']['databases'] = {}
 default['chef-postgresql']['pg_hba'] = [
   {:type => 'local', :db => 'all', :user => 'postgres', :addr => nil, :method => 'ident'},
   {:type => 'local', :db => 'all', :user => 'all', :addr => nil, :method => 'ident'},
