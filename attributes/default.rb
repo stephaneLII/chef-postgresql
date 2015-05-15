846d73900127118b754b4a5e5a08779728f19109
stephaneLII <stephane.lii@informatique.gov.pf>
2015-05-15 10:23:44 -1000

    Intégration du tableau pg_hba vide dans les attributs par défaut.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['postgresql']['database_root_password'] = 'postgres'
 default['postgresql']['config']['listen_addresses'] = '*'
 default['chef-postgresql']['databases'] = {}
+default['chef-postgresql']['pg_hba'] = []
